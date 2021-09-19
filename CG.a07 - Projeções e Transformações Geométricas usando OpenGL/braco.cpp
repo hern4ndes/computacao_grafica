c8ebb8313dab0543b1f9feaa486426f0d9b549ae
Hernandes Erick <erickhernandes4@gmail.com>
2021-09-19 10:26:42 -0300

    added fingers and rotations to arm project

--- CG.a07 - Projeções e Transformações Geométricas usando OpenGL/braco.cpp
+++ CG.a07 - Projeções e Transformações Geométricas usando OpenGL/braco.cpp
@@ -13,127 +13,225 @@
 
 #define ESC 27
 
-static int ombro = 0, cotovelo = 0;
+static int ombro = 0, cotovelo = 0, medio = 0, polegar = 0, indicador = 0, rotacao = 0;
 
 void init(void);
 void keyboard (unsigned char key, int x, int y);
 void display(void);
 void reshape (int w, int h);
 
-int main(int argc, char** argv){
-    /* inicia o GLUT */
-    glutInit(&argc,argv);
+int main(int argc, char** argv)
+{
+	/* inicia o GLUT */
+	glutInit(&argc, argv);
 
-    /* inicia o display usando RGB e double-buffering */
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-    glutInitWindowSize(700,512);
-    glutInitWindowPosition(100,100);
-    glutCreateWindow("Braco Robotico");
+	/* inicia o display usando RGB e double-buffering */
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(700, 512);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Braco Robotico");
 
-    /* Funcao com alguns comandos para a inicializacao do OpenGL; */
-    init ();
+	/* Funcao com alguns comandos para a inicializacao do OpenGL; */
+	init ();
 
-    /* define as funcões de callback */
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-    glutMainLoop();
+	/* define as funcões de callback */
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
 
-  return 0;
+	return 0;
 }
 
 /* Funcao com alguns comandos para a inicializacao do OpenGL; */
-void init(void){
-  glClearColor (1.0, 1.0, 1.0, 1.0); //Limpa a tela com a cor branca;
-  glEnable(GL_DEPTH_TEST); // Habilita o algoritmo Z-Buffer
+void init(void)
+{
+	glClearColor (1.0, 1.0, 1.0, 1.0); //Limpa a tela com a cor branca;
+	glEnable(GL_DEPTH_TEST); // Habilita o algoritmo Z-Buffer
 }
 
-void reshape (int w, int h){
-    /* muda para o modo GL_PROJECTION e reinicia a projecção */
-    glMatrixMode (GL_PROJECTION);
-    glLoadIdentity();
+void reshape (int w, int h)
+{
+	/* muda para o modo GL_PROJECTION e reinicia a projecção */
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity();
 
-    /* define a zona da janela onde se vai desenhar */
-    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+	/* define a zona da janela onde se vai desenhar */
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
 
-    /* Define a forma do "viewing volume" para termos               *
-     * uma projecção de perspectiva (3D).                           *
-     * gluPerpective(angulo,aspecto,ponto_proximo, ponto distante); */
-    gluPerspective(60, (float)w/(float)h, 1.0, 20.0);
-    gluLookAt(0.0,0.0,5.0, 	// posição da câmera (olho) 
-			  0.0,0.0,0.0, 	// centro da cena
-			  0.0,1.0,0.0); // direção de cima 
-    /* muda para o modo GL_MODELVIEW (não pretendemos alterar a projecção
-     * quando estivermos a desenhar a tela) */
-    glMatrixMode (GL_MODELVIEW);
+	/* Define a forma do "viewing volume" para termos               *
+	 * uma projecção de perspectiva (3D).                           *
+	 * gluPerpective(angulo,aspecto,ponto_proximo, ponto distante); */
+	gluPerspective(60, (float)w / (float)h, 1.0, 20.0);
+	gluLookAt(0.0, 0.0, 7.0, 	// posição da câmera (olho)
+			  0.0, 0.0, 0.0, 	// centro da cena
+			  0.0, 1.0, 0.0); // direção de cima
+	/* muda para o modo GL_MODELVIEW (não pretendemos alterar a projecção
+	 * quando estivermos a desenhar a tela) */
+	glMatrixMode (GL_MODELVIEW);
 }
 
-void keyboard (unsigned char key, int x, int y){
-	switch (key) {
-		case 'o': // sentido anti-horário
-			ombro = (ombro + 5) % 360;
-			glutPostRedisplay();
+void keyboard (unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'o': // sentido anti-horário
+		ombro = (ombro + 5) % 360;
+		glutPostRedisplay();
 		break;
-		case 'O': // sentido horário
-			ombro = (ombro - 5) % 360;
-			glutPostRedisplay();
+	case 'O': // sentido horário
+		ombro = (ombro - 5) % 360;
+		glutPostRedisplay();
 		break;
-		case 'c': // sentido anti-horário
+	case 'c': // sentido anti-horário
+		if(cotovelo < 105)
+		{
 			cotovelo = (cotovelo + 5) % 360;
 			glutPostRedisplay();
+		}
 		break;
-		case 'C': // sentido horário
+	case 'C': // sentido horário
+		if(cotovelo > 0)
+		{
 			cotovelo = (cotovelo - 5) % 360;
 			glutPostRedisplay();
+		}
 		break;
-		case ESC:
-			exit(0);
+		// 	indicador
+	case 'i': // sentido anti-horário
+		indicador = (indicador + 5) % 360;
+		glutPostRedisplay();
 		break;
-		default:
+	case 'I': // sentido horário
+		indicador = (indicador - 5) % 360;
+		glutPostRedisplay();
+		break;
+		//polegar
+	case 'p': // sentido anti-horário
+		polegar = (polegar + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'P': // sentido horário
+		polegar = (polegar - 5) % 360;
+		glutPostRedisplay();
+		break;
+		// 	medio
+	case 'm': // sentido anti-horário
+		medio = (medio + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'M': // sentido horário
+		medio = (medio - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'y': // sentido anti-horário
+		rotacao = (rotacao + 5) % 360;
+		glutPostRedisplay();
+		break;
+	case 'Y': // sentido horário
+		rotacao = (rotacao - 5) % 360;
+		glutPostRedisplay();
+		break;
+	case ESC:
+		exit(0);
+		break;
+	default:
 		break;
 	}
 }
 
-void display(void){
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Limpa o Buffer de Cores
-    glLoadIdentity();
+void display(void)
+{
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Limpa o Buffer de Cores
 
-    glPushMatrix();
-        /* origem posicionada no ombro */
-       glTranslatef (-1.0, 0.0, 0.0);
-       glRotatef ((GLfloat) ombro, 0.0, 0.0, 1.0);
-        /* origem posicionada no centro do braço */
-       glTranslatef (1.0, 0.0, 0.0);
 
-        /* ante-braco */
-        glPushMatrix();
-           glScalef (2.0, 0.4, 1.0);
-           glColor3f(0.0,1.0,0.0); 
-           glutSolidCube (1.0);
-            glColor3f(0.0,0.0,0.0);
-            // glutWireCube (1.0);
-        glPopMatrix();
+	glPushMatrix();
+	/* origem posicionada no ombro */
+	glTranslatef (-1.0, 0.0, 0.0);
+	glRotatef ((GLfloat) rotacao, 0.0, 1.0, 0.0);
+	glRotatef ((GLfloat) ombro, 0.0, 0.0, 1.0);
+	// glRotatef (0.0, 0.0,rotacao, 1.0);
+	// /* origem posicionada no centro do braço */
+	glTranslatef (1.0, 0.0, 0.0);
 
-		
-       glPushMatrix();
-         // origem posicionada no cotovelo 
-           glTranslatef (1.0, 0.0, 0.0);
-           glRotatef ((GLfloat) cotovelo, 0.0, 0.0, 1.0);
-           glTranslatef (1.0, 0.0, 0.0);
-         // braco 
-            glPushMatrix();
-               glScalef (2.0, 0.4, 1.0);
-               glColor3f(1.0,1.0,0.0);
-               glutSolidCube (1.0);
-                glColor3f(0.0,0.0,0.0);
-                // glutWireCube (1.0);
-            glPopMatrix();
-        glPopMatrix();
+	/* ante-braco */
+	glPushMatrix();
+	glScalef (2.0, 0.4, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glutSolidCube (1.0);
+	glPopMatrix();
 
-    /* origem volta para o sistema de coordenadas original */
-    glPopMatrix();
+//
+	glPushMatrix();
+	// origem posicionada no cotovelo
+	glTranslatef (1.0, 0.0, 0.0);
+	glRotatef ((GLfloat) cotovelo, 0.0, 0.0, 1.0);
+	glTranslatef (1.0, 0.0, 0.0);
+	// braco
+	glPushMatrix();
+	glScalef (2.0, 0.4, 1.0);
+	glColor3f(0.0, 1.0, 0.0);
+	glutSolidCube (1.0);
+	glPopMatrix();
 
-    /* Troca os buffers, mostrando o que acabou de ser desenhado */
-    glutSwapBuffers();
+	// origem posicionada no cotovelo
+	glPushMatrix();
+	// origem posicionada no cotovelo
+	glTranslatef (1.0, 0.0, 0.0);
+	glRotatef ((GLfloat) indicador, 0.0, 0.0, 1.0);
+	glTranslatef (0.5, 0.25, 0.25);
+	// braco
+	glPushMatrix();
+	glScalef (1, 0.25, 0.5);
+	glColor3f(0.0, 0.0, 1.0);
+	glutSolidCube (1.0);
+	glPopMatrix();
+	// origem posicionada no cotovelo
+	glPopMatrix();
+	
+	
+	
+	
+		glPushMatrix();
+	// origem posicionada no cotovelo
+	glTranslatef (1.0, 0.0, 0.0);
+	glRotatef ((GLfloat) medio, 0.0, 0.0, 1.0);
+	glTranslatef (0.5, 0.25, -0.25);
+	// braco
+	glPushMatrix();
+	glScalef (1, 0.25, 0.5);
+	glColor3f(0.0, 1.0, 1.0);
+	glutSolidCube (1.0);
+	glPopMatrix();
+	// origem posicionada no cotovelo
+	glPopMatrix();
+	
+	
+	
+		glPushMatrix();
+	// origem posicionada no cotovelo
+	glTranslatef (1.0, 0.0, 0.0);
+	glRotatef ((GLfloat) polegar, 0.0, 0.0, 1.0);
+	glTranslatef (0.5, -0.25, 0.0);
+	// braco
+	glPushMatrix();
+	glScalef (1, 0.25, 0.5);
+	glColor3f(1.0, 1.0, 0.0);
+	glutSolidCube (1.0);
+	glPopMatrix();
+	// origem posicionada no cotovelo
+	glPopMatrix();
+	
+	
+
+	glPopMatrix();
+	
+
+
+	/* origem volta para o sistema de coordenadas original */
+	glPopMatrix();
+
+	/* Troca os buffers, mostrando o que acabou de ser desenhado */
+	glutSwapBuffers();
 }
 
